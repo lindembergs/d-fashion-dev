cd62fc008e39800333c5bca65f397b0b80c89529
Lindemberg <lindembergparaiba@gmail.com>
2024-02-17 21:30:32 -0300

    style- ajuste no carrossel, mas ainda com bugs

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,31 @@
-document.addEventListener("DOMContentLoaded",  () => {
-    let swiper = new Swiper(".mySwiper", {
+
+  let swiper = new Swiper(".mySwiper", {
       slidesPerView: 5,
       spaceBetween: 30,
       pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+          el: ".swiper-pagination",
+          clickable: true,
+          type: "bullets"
       },
       grabCursor: true,
       loop: true,
-    });
+      simulateTouch: true,
+      breakpoints: {
+          480: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+              // ... outras configurações para dispositivos móveis ...
+          },
+          768: {
+              slidesPerView: 2,
+              spaceBetween: 15,
+              // ... outras configurações para tablets ...
+          },
+          1024: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+              // ... outras configurações para tablets maiores ou desktops menores ...
+          }
+      }
   });
+
