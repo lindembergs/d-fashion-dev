e082bb1b4053135f0bfc5b1b76b9e762f2c65d68
Lindemberg <lindembergparaiba@gmail.com>
2024-02-21 10:55:35 -0300

    -style inicio da estilizacao da pagina de produtos e adicao dos assets

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,84 +1,69 @@
-fetch('dados.json')
-      .then(response => response.json())
-      .then(jsonData => {
-        function createCarousel(containerId, items) {
-          const container = document.getElementById(containerId);
-          const swiperWrapper = document.createElement('div');
-          swiperWrapper.classList.add('swiper-wrapper');
+document.addEventListener('DOMContentLoaded',() => {
+  fetch('dados.json')
+  .then(response => response.json())
+  .then(data => {
+    function createCarousel(containerId, items) {
+      const container = document.getElementById(containerId);
+      const swiperWrapper = document.createElement('div');
+      swiperWrapper.classList.add('swiper-wrapper');
+     
 
-          items.forEach(item => {
-            const slide = document.createElement('div');
-            slide.classList.add('swiper-slide', 'card');
-            console.log(item)
+      items.forEach(item => {
+        const slide = document.createElement('div');
+        slide.classList.add('swiper-slide', 'card');
+        console.log(item)
 
-            slide.innerHTML = `<div class="card">
-            <img src="${item.image}" alt="${item.product_name}" />
-                               <div class="card-info">
-                                  <p>${item.product_name}</p>
-                                  <div class="price-btn">
-                                    <span>${item.current_price}</span>
-                                    <span>${item.old_price}</span>
-                                    <button><img src="src/assets/icons/+.svg" alt="" /></button>
-                                  </div>
-                               </div>`;
+        slide.innerHTML = `<img src="${item.image}" alt="${item.product_name}" />
+                           <div class="card-info">
+                              <p>${item.product_name}</p>
+                              <div class="price-btn">
+                                <span>${item.current_price}</span>
+                                <span>${item.old_price}</span>
+                                <button><a href= "./product.html"><img src="src/assets/icons/+.svg" alt="" /><a/></button>
+                              </div>
+                           </div>`;
+                           console.log(slide)
 
-            swiperWrapper.appendChild(slide);
-          });
-
-          container.appendChild(swiperWrapper);
-
-          new Swiper(`#${containerId}`, {
-            slidesPerView: 5,
-            spaceBetween: 30,
-            // loop: true,
-            pagination: {
-                el: ".swiper-pagination",
-                clickable: true,
-              },
-            navigation: {
-              nextEl: `#${containerId}-next`,
-              prevEl: `#${containerId}-prev`,
-            },
-            
-          });
-        }
-
-        const conjuntos = jsonData.find(category => category.class === 'Conjunto');
-        const tshirts = jsonData.find(category => category.class === 'T-shirts');
-        const casacos = jsonData.find(category => category.class === 'Casacos');
-
-        createCarousel('conjuntos-carousel', conjuntos.product);
-        createCarousel('tshirts-carousel', tshirts.product);
-        createCarousel('casacos-carousel', casacos.product);
+        swiperWrapper.appendChild(slide);
       });
 
-//   let swiper = new Swiper(".mySwiper", {
-//       slidesPerView: 5,
-//       spaceBetween: 30,
-//       pagination: {
-//           el: ".swiper-pagination",
-//           clickable: true,
-//           type: "bullets"
-//       },
-//       grabCursor: true,
-//       loop: true,
-//       simulateTouch: true,
-//       breakpoints: {
-//           480: {
-//               slidesPerView: 1,
-//               spaceBetween: 10,
-//               // ... outras configurações para dispositivos móveis ...
-//           },
-//           768: {
-//               slidesPerView: 2,
-//               spaceBetween: 15,
-//               // ... outras configurações para tablets ...
-//           },
-//           1024: {
-//               slidesPerView: 3,
-//               spaceBetween: 20,
-//               // ... outras configurações para tablets maiores ou desktops menores ...
-//           }
-//       }
-//   });
+      container.appendChild(swiperWrapper);
 
+      new Swiper(`#${containerId}`, {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: `.swiper-pagination-${containerId}`,
+          clickable: true,
+        },
+        navigation: {
+          nextEl: `#${containerId}-next`,
+          prevEl: `#${containerId}-prev`,
+        },
+        breakpoints: {
+          480: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          }
+        }
+      });
+    }
+
+    const conjuntos = data.find(category => category.class === 'Conjunto');
+    const tshirts = data.find(category => category.class === 'T-shirts');
+    const casacos = data.find(category => category.class === 'Casacos');
+
+    createCarousel('conjuntos-carousel', conjuntos.product);
+    createCarousel('tshirts-carousel', tshirts.product);
+    createCarousel('casacos-carousel', casacos.product);
+  });
+})
