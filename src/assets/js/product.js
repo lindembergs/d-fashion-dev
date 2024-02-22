0ddb27e433945f02e6d52ecef5ce60cca6412fa5
Lindemberg <lindembergparaiba@gmail.com>
2024-02-21 22:26:09 -0300

    bug fix 2

--- src/assets/js/product.js
+++ src/assets/js/product.js
@@ -1,1 +1,19 @@
 
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productName = urlParams.get('productName');
+    const currentPrice = urlParams.get('currentPrice');
+    const oldPrice = urlParams.get('oldPrice');
+    const image = urlParams.get('image');
+   
+    const data = getData()
+    console.log(data)
+
+    document.querySelector('.left-container img').src = decodeURIComponent(image);
+    document.querySelector('.right-container p').textContent = productName;
+    document.querySelector('.price span').textContent = currentPrice;
+    document.querySelector('.old-price').textContent = oldPrice;
+    document.querySelector('.right-container p').textContent = productName;
+    document.querySelector('.price span').textContent = currentPrice;
+    document.querySelector('.old-price').textContent = oldPrice;
+  });
