90ec38dd85f91f6cb6928a7fc37d561d2e5a0f2a
Lindemberg <lindembergparaiba@gmail.com>
2024-02-27 15:31:58 -0300

    adicao da url dinamica para melhor atualizacao no carrinho de compras

--- src/assets/js/cart.js
+++ src/assets/js/cart.js
@@ -1,74 +1,31 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-  // Aqui você pode carregar os itens do carrinho e exibi-los na página do carrinho
-  loadCartItems();
 
-  function loadCartItems() {
-    const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
-
-    // Exemplo: exibir os itens na console
-    console.log(cartItems);
-    // Agora você deve adicionar lógica para exibir os itens na página do carrinho
-  }
-});
+// const addToCartBtn = document.querySelector('#add-cart-btn')
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    // Verifique se há parâmetros na URL
-    const urlParams = new URLSearchParams(window.location.search);
-    const productName = urlParams.get('productName');
-    const currentPrice = urlParams.get('currentPrice');
-    const oldPrice = urlParams.get('oldPrice');
-    const image = urlParams.get('image');
-    const color = urlParams.get('color');
-    const size = urlParams.get('size');
-
-    // Se houver parâmetros, atualize o link do carrinho
-    if (productName && currentPrice && oldPrice && image && color && size) {
-      const cartLink = document.getElementById('openModalBtn');
-      const cartHref = `./product.html?productName=${encodeURIComponent(productName)}&currentPrice=${encodeURIComponent(currentPrice)}&oldPrice=${encodeURIComponent(oldPrice)}&image=${encodeURIComponent(image)}&color=${encodeURIComponent(color)}&size=${encodeURIComponent(size)}`;
-      cartLink.parentElement.setAttribute('href', cartHref);
-    }
-  });
-
-
-
-
-
-
-
-
-const addToCartBtn = document.querySelector('#add-cart-btn')
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const productName = urlParams.get('productName');
-    const currentPrice = urlParams.get('currentPrice');
-    const oldPrice = urlParams.get('oldPrice');
-    const image = urlParams.get('image');
-    // modal
-    const color = urlParams.get('color');
-    const size = urlParams.get('size');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const urlParams = new URLSearchParams(window.location.search);
+//     const productName = urlParams.get('productName');
+//     const currentPrice = urlParams.get('currentPrice');
+//     const oldPrice = urlParams.get('oldPrice');
+//     const image = urlParams.get('image');
+//     // modal
+//     const color = urlParams.get('color');
+//     const size = urlParams.get('size');
    
 
-    document.querySelector('.left-container img').src = decodeURIComponent(image);
-    document.querySelector('.right-container p').textContent = productName;
-    document.querySelector('.price span').textContent = currentPrice;
-    document.querySelector('.old-price').textContent = oldPrice;
-    document.querySelector('.right-container p').textContent = productName;
-    document.querySelector('.price span').textContent = currentPrice;
-    document.querySelector('.old-price').textContent = oldPrice;
-    // modal
-    document.querySelector('.product-modal-img').src = decodeURIComponent(image);
-    document.querySelector('.product-description').textContent = productName;
-    document.querySelector('.color-modal').textContent = encodeURIComponent(color)
-    document.querySelector('.selected-size').textContent = encodeURIComponent(size)
-  });
+//     document.querySelector('.left-container img').src = decodeURIComponent(image);
+//     document.querySelector('.right-container p').textContent = productName;
+//     document.querySelector('.price span').textContent = currentPrice;
+//     document.querySelector('.old-price').textContent = oldPrice;
+//     document.querySelector('.right-container p').textContent = productName;
+//     document.querySelector('.price span').textContent = currentPrice;
+//     document.querySelector('.old-price').textContent = oldPrice;
+//     // modal
+//     document.querySelector('.product-modal-img').src = decodeURIComponent(image);
+//     document.querySelector('.product-description').textContent = productName;
+//     document.querySelector('.color-modal').textContent = encodeURIComponent(color)
+//     document.querySelector('.selected-size').textContent = encodeURIComponent(size)
+//   });
   
-  const addToCart = () => {
-   alert('Produto adicionado ao carrinho')
-  }
-  
-  addToCartBtn.addEventListener('click', addToCart)
   
--- src/assets/js/product.js
+++ src/assets/js/product.js
@@ -1,46 +1,79 @@
 
 const addToCartBtn = document.querySelector('#add-cart-btn')
+const openCartBtn = document.querySelector('#open-cart-btn')
+const colorOptionTargets = document.querySelectorAll('.color-option-target');
+let selectedColor = ''
+const sizeOptionTargets = document.querySelectorAll('.size-option-target');
+let selectedSize = ''
+
+for (let i = 0; i < colorOptionTargets.length; i++) {
+  const colorOption = colorOptionTargets[i];
+  
+  colorOption.addEventListener('click', (e) => {
+   selectedColor = e.target.getAttribute('data-color');
+    console.log(selectedColor);
+    let productColor = document.querySelector("#product-color")
+    productColor.textContent = `Cor: ${selectedColor}`
+  });
+
+  
+}
+for (let i = 0; i < sizeOptionTargets.length; i++) {
+  const sizeOption = sizeOptionTargets[i];
+  
+  sizeOption.addEventListener('click', (e) => {
+   selectedSize = e.target.textContent
+    console.log(selectedSize);
+  });
+
+  
+}
+
+
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const productName = urlParams.get('productName');
-    const currentPrice = urlParams.get('currentPrice');
-    const oldPrice = urlParams.get('oldPrice');
-    const image = urlParams.get('image');
-
-    document.querySelector('.left-container img').src = decodeURIComponent(image);
-    document.querySelector('.right-container p').textContent = productName;
-    document.querySelector('.price span').textContent = currentPrice;
-    document.querySelector('.old-price').textContent = oldPrice;
-    document.querySelector('.right-container p').textContent = productName;
-    document.querySelector('.price span').textContent = currentPrice;
-    document.querySelector('.old-price').textContent = oldPrice;
-  });
-
- 
+  const urlParams = new URLSearchParams(window.location.search);
+  const productName = urlParams.get('productName');
+  const currentPrice = urlParams.get('currentPrice');
+  const oldPrice = urlParams.get('oldPrice');
+  const image = urlParams.get('image');
   
-  const addToCart = () => {
-   alert('Produto adicionado ao carrinho')
+  document.querySelector('.left-container img').src = decodeURIComponent(image);
+  document.querySelector('.right-container p').textContent = productName;
+  document.querySelector('.price span').textContent = currentPrice;
+  document.querySelector('.old-price').textContent = oldPrice;
+  document.querySelector('.right-container p').textContent = productName;
+  document.querySelector('.price span').textContent = currentPrice;
+  document.querySelector('.old-price').textContent = oldPrice;
+});
+
+
+
+const addToCart = () => {
+  alert('Produto adicionado ao carrinho')
+}
+
+addToCartBtn.addEventListener('click', addToCart)
+
+function attCartUrl () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const productName = urlParams.get('productName');
+  const currentPrice = urlParams.get('currentPrice');
+  const image = urlParams.get('image');
+  const color = urlParams.get('color');
+  const size = urlParams.get('size');
+  
+  
+  
+  if (productName && currentPrice && image && color && size) {
+    const cartLink = document.getElementById('openModalBtn');
+    const cartHref = `./cart.html?productName=${encodeURIComponent(productName)}&currentPrice=${encodeURIComponent(currentPrice)}&image=${encodeURIComponent(image)}&color=${encodeURIComponent(selectedColor)}&size=${encodeURIComponent(selectedSize)}`;
+    cartLink.parentElement.setAttribute('href', cartHref);
   }
-  
-  addToCartBtn.addEventListener('click', addToCart)
-  
- function atualizaCartUrl () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const productName = urlParams.get('productName');
-    const currentPrice = urlParams.get('currentPrice');
-    const oldPrice = urlParams.get('oldPrice');
-    const image = urlParams.get('image');
-    const color = urlParams.get('color');
-    const size = urlParams.get('size');
-
-    if (productName && currentPrice && oldPrice && image && color && size) {
-      const cartLink = document.getElementById('openModalBtn');
-      const cartHref = `./product.html?productName=${encodeURIComponent(productName)}&currentPrice=${encodeURIComponent(currentPrice)}&oldPrice=${encodeURIComponent(oldPrice)}&image=${encodeURIComponent(image)}&color=${encodeURIComponent(color)}&size=${encodeURIComponent(size)}`;
-      cartLink.parentElement.setAttribute('href', cartHref);
-    }
-  }
+}
+openCartBtn.addEventListener('click', attCartUrl)
 
 
 
